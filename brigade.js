997a29d741f5bee10d99a2700fb8d257e6d49d54
Pengzhi Sun <pengzhisun@live.com>
2019-02-27 15:54:01 +0800

    Add debug code to brigade.js file.

--- brigade.js
+++ brigade.js
@@ -2,6 +2,8 @@ function kubeJobRunner (config, k) {
     k.storage.enabled = false
     k.image = "lachlanevenson/k8s-kubectl:v1.8.2"
     k.tasks = [
+        `echo brigade job running...`,
+        `kubectl get nodes`,
         `kubectl set image deployment/heroes-web-deploy heroes-web-cntnr=pensuncb19acr.azurecr.io/azureworkshop/rating-web:${config.get("imageTag")}`
     ]
 }
